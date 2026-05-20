1b36598d7aa9a66b10fe4ef74081a21631107905
Razan Khalaf <roznah2001@gmail.com>
2026-05-21 02:15:09 +0400

    fix: formatCurrency to handle string from pg

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,3 +1,3 @@
-export function formatCurrency(amount: number) {
-  return `د.إ ${amount.toFixed(2)}`;
+export function formatCurrency(amount: number | string) {
+  return `د.إ ${Number(amount).toFixed(2)}`;
 }
